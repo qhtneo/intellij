5142725af3efeb873dac0a0c406278f774a4fc28
inns21 <inns2110@gmail.com>
2023-04-16 23:29:09 +0900

    feat board

--- src/main/java/com/project/trip/controller/BoardController.java
+++ src/main/java/com/project/trip/controller/BoardController.java
@@ -1,12 +1,70 @@
 package com.project.trip.controller;
 
+import com.project.trip.service.BoardService;
+import com.project.trip.service.MemberService;
+import com.project.trip.vo.Board;
+import com.project.trip.vo.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 public class BoardController {
+
+    @Autowired
+    private BoardService bService;
+
+    @Autowired
+    private MemberService mService;
+
     @GetMapping("/board")
-    public String board(){
-        return "board/BoardList";
+    public String board(Model model){
+        List<Board> boardList = bService.selectAllBoard();
+        log.debug("list size {}  ", boardList.size());
+
+        model.addAttribute("boardList", boardList);
+
+        return "board/boardList";
+    }
+
+    @GetMapping("/write")
+    public String write(){
+        return "board/write";
+    }
+
+    @PostMapping("/write")
+    public String write(Board board, @AuthenticationPrincipal UserDetails user) {
+        log.debug("write(Board)");
+        log.debug("Board : {}", board);
+        String userId = user.getUsername();
+        Member member  = mService.findOneMember(userId);
+        board.setUserNickname((member.getUserNickname()));
+        board.setUserNo(member.getUserNo());
+        bService.write(board);
+        return "redirect:/board";
+    }
+
+    @GetMapping("/read")
+    public String read(Model model, int boardNo) {
+        log.debug("read()");
+        log.debug("boardNum : {}", boardNo);
+
+        // service 호출
+        // 글읽기와 조회수가 합쳐짐
+        Board board = bService.read(boardNo);
+        // model 객체에 글 정보 담기
+        model.addAttribute("board", board);
+
+        return "board/read";
     }
 }
--- src/main/java/com/project/trip/service/BoardServiceImpl.java
+++ src/main/java/com/project/trip/service/BoardServiceImpl.java
@@ -1,4 +1,30 @@
 package com.project.trip.service;
 
-public class BoardServiceImpl {
+import com.project.trip.dao.BoardDao;
+import com.project.trip.vo.Board;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Autowired
+    private BoardDao bDao;
+
+    @Override
+    public int write(Board board) {
+        return bDao.insertBoard(board);
+    }
+
+    @Override
+    public List<Board> selectAllBoard() {
+        return bDao.selectAllBoard();
+    }
+
+    @Override
+    public Board read(int boardNo) {
+        return bDao.selectOneBoard(boardNo);
+    }
 }
