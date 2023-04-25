45f82910cdcfab7509430ee98407b2fbabceed35
dh2wo <osj4927@gmail.com>
2023-04-25 09:41:45 +0900

    feat:reply_final_bugFix2

--- src/main/java/com/project/trip/controller/MainController.java
+++ src/main/java/com/project/trip/controller/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
 	//메인화면 띄우기
 	@GetMapping({"", "/"}) //localhost8888 enter or localhost8888/ enter
 	public String index() {
-		return "main2";
+		return "main";
 	}
 
 	@PostMapping("")
