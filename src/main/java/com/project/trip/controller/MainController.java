57170e052214bd4b8190a8ed4401c5851bcb8eca
dh2wo <osj4927@gmail.com>
2023-04-27 09:46:55 +0900

    update:mapAPI

--- src/main/java/com/project/trip/controller/MainController.java
+++ src/main/java/com/project/trip/controller/MainController.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class MainController {
 	
 	//메인화면 띄우기
-	@GetMapping({"", "/"}) //localhost8888 enter or localhost8888/ enter
-	public String index() {
-		return "main";
-	}
+//	@GetMapping({"", "/"}) //localhost8888 enter or localhost8888/ enter
+//	public String index() {
+//		return "main";
+//	}
 
-	@PostMapping("")
-	public String address(Model model){
+	@GetMapping({"", "/"}) //localhost8888 enter or localhost8888/ enter
+	public String index(Model model) {
 		String address = "광주광역시 광산구 무진대로 282";
 		model.addAttribute("address", address);
-		return "main2";
+		return "main";
 	}
 }
