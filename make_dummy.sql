cbc8c60b8c74554bb11bd414e0dd3a708d520e1f
dh2wo <osj4927@gmail.com>
2023-06-07 23:44:46 +0900

    Update:adminPageLink

--- make_dummy.sql
+++ make_dummy.sql
@@ -5,24 +5,24 @@ CREATE PROCEDURE makedummy()
 BEGIN
     DECLARE i INT DEFAULT 1; 
     WHILE (i <= 100) DO 
-        INSERT INTO BOARD(user_no, user_nickname, board_title, board_summary,local_category)
-        VALUE(1,"aaaa","dummy","bulabula","서울특별시");
+        INSERT INTO BOARD(user_no, user_nickname, board_title, board_summary, local_category)
+        VALUES(1, 'aaaa', 'dummy', 'bulabula', '서울특별시');
         SET i = i + 1;
     END WHILE;
     
-	UPDATE BOARD SET local_category = '전라남도' 		WHERE board_no < 100; 
+	UPDATE BOARD SET local_category = '전라남도' 		WHERE board_no < 100;
 	UPDATE BOARD SET local_category = '충청남도' 		WHERE board_no < 88;
 	UPDATE BOARD SET local_category = '대전광역시' 	WHERE board_no < 75;
 	UPDATE BOARD SET local_category = '경상북도' 		WHERE board_no < 50;
-	UPDATE BOARD SET local_category = '제주특별자치도' 	WHERE board_no < 43;
+	UPDATE BOARD SET local_category = '제주특별자치도' WHERE board_no < 43;
 	UPDATE BOARD SET local_category = '강원도'		WHERE board_no < 35;
-	UPDATE BOARD SET local_category = '대구광역시' 	WHERE board_no < 25; 
+	UPDATE BOARD SET local_category = '대구광역시' 	WHERE board_no < 25;
 	UPDATE BOARD SET local_category = '부산광역시' 	WHERE board_no < 17;
-	UPDATE BOARD SET local_category = '서울특별시' 	WHERE board_no < 10; 
+	UPDATE BOARD SET local_category = '서울특별시' 	WHERE board_no < 10;
     
-	INSERT INTO Member (USER_id, USER_PW, USER_NICKNAME, USER_EMAIL)
+	INSERT INTO MEMBER (USER_id, USER_PW, USER_NICKNAME, USER_EMAIL)
 			VALUES ('admin01', '{bcrypt}$2a$10$JD5PYjwzAK/kAcgFtXDeX.jFMEtBt8opv/tEDO5bXNYTxSVh6AqJi', 'Admin', 'admin@admin.com');
-	INSERT INTO Member (USER_id, USER_PW, USER_NICKNAME, USER_EMAIL)
+	INSERT INTO MEMBER (USER_id, USER_PW, USER_NICKNAME, USER_EMAIL)
 			VALUES ('aaaa', '{bcrypt}$2a$10$EU.KFqAZHtq.QENqncjpKeIcxuqwXY9LvzC45/oOsZlT3zKr.FjUe', 'aaaa', 'aaaa@aaaa.com');
 	UPDATE MEMBER SET rolename ='ROLE_ADMIN' where user_id ='admin01';
 
--- src/main/java/com/project/trip/dao/BoardDao.java
+++ src/main/java/com/project/trip/dao/BoardDao.java
@@ -3,6 +3,7 @@ package com.project.trip.dao;
 import com.project.trip.vo.Board;
 import com.project.trip.vo.Member;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 import java.util.List;
@@ -15,7 +16,9 @@ public interface BoardDao {
     int updateBoard(Board board);
     int deleteBoard(int boardNo);
 //    List<Board> selectAllBoard(RowBounds rb);
-    int countBoard(String keyword, String category, String localCategory);
+//    int countBoard(String keyword, String category, String localCategory);
+    int countBoard(@Param("keyword") String keyword, @Param("category") String category, @Param("localCategory") String localCategory);
+
     List<Board> selectBoardByKeyword(Map<String, Object> map, RowBounds rb);
     List<Board> selectBoardById(String userId);
     void updateRecommend(int boardNo);
